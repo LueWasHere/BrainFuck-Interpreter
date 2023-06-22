1446c2fd3b1c6638679e5663a347c2f7e08fdca8
LueWasHere <82827429+LueWasHere@users.noreply.github.com>
2023-06-21 19:41:57 -0600

    Update main.py
    Refactor code and ensure the correct amount of spacing is used in the log file

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from sys import argv
 whitespace = "   "
-
 class ProgramError(Exception):
-    def __init__(self, message="ProgramError: Provide an input file!\n\t(Executable Name)\n                      ^ File name goes here"):
+    def __init__(self, message="ProgramError: Provide an input file!\n\t(Executable Name)\n                          ^ File name goes here"):
         self.message = message
         super().__init__(self.message)
 class LoopError(Exception):
@@ -15,14 +14,37 @@ if len(argv) > 1:
         BFFile.close()
 else:
     raise ProgramError
-
 ops = ["+", "-", ".", ",", "[", "]", "<", ">"]
 pointer = 0
 tape = [0]
 prog_ops = ["-so", "--saveoutput"]
 savefile = ""
 to_save = ""
-
+def get_extra_whitespace(tape, pointer) -> int:
+    extra = 0
+    for i,num in enumerate(tape):
+        if i == pointer:
+            break
+        extra += len(str(num))-1
+    return extra
+def add_to_save(string: str, error: bool=False, error_type=None) -> None:
+    global savefile
+    global to_save
+    if savefile != "":
+        if error:
+            to_save += "Broke at error."
+            try:
+                with open(savefile, "w") as SaveFile:
+                    SaveFile.write(to_save)
+                    SaveFile.close()
+            except FileNotFoundError:
+                with open(savefile, "a") as SaveFile:
+                    SaveFile.write(to_save)
+                    SaveFile.close()
+            raise error_type
+        to_save += string
+        return
+    return
 for i in range(0, len(argv)):
     if argv[i] in prog_ops:
         if prog_ops[prog_ops.index(argv[i])] == "-so" or prog_ops[prog_ops.index(argv[i])] == "--saveoutput":
@@ -41,94 +63,63 @@ while i != len(content):
                 tape.insert(0, 0)
             else:
                 pointer -= 1
-            if savefile != "":
-                to_save += "Decrement pointer:\n"+str(tape)+f"\n {whitespace * pointer}^\n"
+            add_to_save("Decrement pointer:\n"+str(tape)+f"\n {whitespace * pointer}{' ' * get_extra_whitespace(tape, pointer)}^\n")
         elif content[i] == ">":
             if pointer == len(tape)-1:
                 pointer += 1
                 tape.append(0)
             else:
                 pointer += 1
-            if savefile != "":
-                to_save += "Increment pointer:\n"+str(tape)+f"\n {whitespace * pointer}^ -> (Line {ln} Char {char})\n"
+            add_to_save("Increment pointer:\n"+str(tape)+f"\n {whitespace * pointer}{' ' * get_extra_whitespace(tape, pointer)}^ -> (Line {ln} Char {char})\n")
         elif content[i] == "+":
             tape[pointer] += 1
-            if savefile != "":
-                to_save += "Increment value at pointer:\n"+str(tape)+f"\n {whitespace * pointer}^ -> (Line {ln} Char {char})\n{whitespace * pointer} | {tape[pointer]-1} -> {tape[pointer]}\n"
+            add_to_save("Increment value at pointer:\n"+str(tape)+f"\n {whitespace * pointer}{' ' * get_extra_whitespace(tape, pointer)}^ -> (Line {ln} Char {char})\n{whitespace * pointer}{' ' * get_extra_whitespace(tape, pointer)} | {tape[pointer]-1} -> {tape[pointer]}\n")
         elif content[i] == "-":
             tape[pointer] -= 1
-            if savefile != "":
-                to_save += "Decrement value at pointer:\n"+str(tape)+f"\n {whitespace * pointer}^ -> (Line {ln} Char {char})\n{whitespace * pointer} | {tape[pointer]+1} -> {tape[pointer]}\n"
+            add_to_save("Decrement value at pointer:\n"+str(tape)+f"\n {whitespace * pointer}{' ' * get_extra_whitespace(tape, pointer)}^ -> (Line {ln} Char {char})\n{whitespace * pointer}{' ' * get_extra_whitespace(tape, pointer)} | {tape[pointer]+1} -> {tape[pointer]}\n")
         elif content[i] == "[":
             if tape[pointer] == 0:
                 while content[i] != "]":
                     i += 1
                     if i == len(content)-1:
-                        if savefile != "":
-                            to_save += "Broke at error."
-                            try:
-                                with open(savefile, "w") as SaveFile:
-                                    SaveFile.write(to_save)
-                                    SaveFile.close()
-                            except FileNotFoundError:
-                                with open(savefile, "a") as SaveFile:
-                                    SaveFile.write(to_save)
-                                    SaveFile.close()
-                        raise LoopError
+                        add_to_save("", error=True, error_type=LoopError)
             else:
                 pass
-            if savefile != "":
-                try:
-                    to_save += "Begin Loop at pointer:\n"+str(content[i-5:i+5]).replace("\n", "\\n")+f"\n  {whitespace}^ -> (Line {ln} Char {char})\n"
-                except:
-                    to_save += "Begin Loop at pointer:\n"+str(content[i:i+1]).replace("\n", "\\n")+f" . . . \n  {whitespace}^ -> (Line {ln} Char {char})\n"
+            try:
+                add_to_save("Begin Loop at pointer:\n"+str(content[i-5:i+5]).replace("\n", "\\n")+f"\n  {whitespace}^ -> (Line {ln} Char {char})\n")
+            except IndexError:
+                add_to_save("Begin Loop at pointer:\n"+str(content[i:i+1]).replace("\n", "\\n")+f" . . . \n  {whitespace}^ -> (Line {ln} Char {char})\n")
         elif content[i] == "]":
-            if savefile != "":
-                try:
-                    to_save += "Check Loop at pointer:\n"+str(content[i-5:i+5]).replace("\n", "\\n")+f"\n  {whitespace}^ -> (Line {ln} Char {char})\n"
-                except:
-                    to_save += "Check Loop at pointer:\n"+str(content[i:i+1]).replace("\n", "\\n")+f" . . .\n  {whitespace}^ -> (Line {ln} Char {char})\n"
+            try:
+                add_to_save("Check Loop at pointer:\n"+str(content[i-5:i+5]).replace("\n", "\\n")+f"\n  {whitespace}^ -> (Line {ln} Char {char})\n")
+            except:
+                add_to_save("Check Loop at pointer:\n"+str(content[i:i+1]).replace("\n", "\\n")+f" . . .\n  {whitespace}^ -> (Line {ln} Char {char})\n")
             if tape[pointer] == 0:
-                if savefile != "":
-                    try:
-                        to_save += "End Loop at pointer:\n"+str(content[i-5:i+5]).replace("\n", "\\n")+f"\n  {whitespace}^ -> (Line {ln} Char {char})\n"
-                    except:
-                        to_save += "End Loop at pointer:\n"+str(content[i:i+1]).replace("\n", "\\n")+f" . . .\n  {whitespace}^ -> (Line {ln} Char {char})\n"
+                try:
+                    add_to_save("End Loop at pointer:\n"+str(content[i-5:i+5]).replace("\n", "\\n")+f"\n  {whitespace}^ -> (Line {ln} Char {char})\n")
+                except:
+                    add_to_save("End Loop at pointer:\n"+str(content[i:i+1]).replace("\n", "\\n")+f" . . .\n  {whitespace}^ -> (Line {ln} Char {char})\n")
             else:
-                if savefile != "":
-                    try:
-                        to_save += "Re-Enter Loop at pointer:\n"+str(content[i-5:i+5]).replace("\n", "\\n")+f"\n  {whitespace}^ -> (Line {ln} Char {char})\n"
-                    except:
-                        to_save += "Re-Enter at pointer:\n"+str(content[i:i+1]).replace("\n", "\\n")+f" . . .\n  {whitespace}^ -> (Line {ln} Char {char})\n"
+                try:
+                    add_to_save("Re-Enter Loop at pointer:\n"+str(content[i-5:i+5]).replace("\n", "\\n")+f"\n  {whitespace}^ -> (Line {ln} Char {char})\n")
+                except:
+                    add_to_save("Re-Enter at pointer:\n"+str(content[i:i+1]).replace("\n", "\\n")+f" . . .\n  {whitespace}^ -> (Line {ln} Char {char})\n")
                 while content[i] != "[":
                     i -= 1
                     char -= 1
                     if char < 0:
                         ln -= 1
                         char = len(content.split('\n')[ln-1])
-                if savefile != "":
-                    to_save += f"{whitespace}  | -> Re-Enters at: "+str(content[i:i+1]).replace("\n", "\\n")+f" . . . \t -> (Line {ln} Char {char})\n                        ^\n"
+                add_to_save(f"{whitespace}  | -> Re-Enters at: "+str(content[i:i+1]).replace("\n", "\\n")+f" . . . \t -> (Line {ln} Char {char})\n                        ^\n")
         elif content[i] == ".":
-            if tape[pointer] > 0:
+            if tape[pointer] > -1:
                 print(chr(tape[pointer]), end="")
             else:
-                if savefile != "":
-                    to_save += "Broke at error."
-                    try:
-                        with open(savefile, "w") as SaveFile:
-                            SaveFile.write(to_save)
-                            SaveFile.close()
-                    except FileNotFoundError:
-                        with open(savefile, "a") as SaveFile:
-                            SaveFile.write(to_save)
-                            SaveFile.close()
-                raise ValueError(f'Error while printing from tape: Cannot print values less than zero! (Line {ln} Char {char})')
-            if savefile != "":
-                to_save += "Printed value at pointer:\n"+str(tape)+f"\n {whitespace * pointer}^ -> (Line {ln} Char {char})\n{whitespace * pointer} | {tape[pointer]} -> CONSOLE\n"
+                add_to_save("", error=True, error_type=ValueError(f'Error while printing from tape: Cannot print values less than zero! (Line {ln} Char {char})'))
+            add_to_save("Printed value at pointer:\n"+str(tape)+f"\n {whitespace * pointer}{' ' * get_extra_whitespace(tape, pointer)}^ -> (Line {ln} Char {char})\n{whitespace * pointer}{' ' * get_extra_whitespace(tape, pointer)} | {tape[pointer]} -> CONSOLE\n")
         elif content[i] == ",":
             print(chr(tape[pointer]), end="")
-            if savefile != "":
-                to_save += "Put input value at pointer:\n"+str(tape)+f"\n {whitespace * pointer}^ -> (Line {ln} Char {char})\n{whitespace * pointer} | CONSOLE -> {tape[pointer]}\n"
+            add_to_save("Put input value at pointer:\n"+str(tape)+f"\n {whitespace * pointer}{' ' * get_extra_whitespace(tape, pointer)}^ -> (Line {ln} Char {char})\n{whitespace * pointer}{' ' * get_extra_whitespace(tape, pointer)} | CONSOLE -> {tape[pointer]}\n")
         #elif 
     i += 1
 if savefile != "":
